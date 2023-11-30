cb1b98405d748defd7a49986c9f6bbfe94d5c801
KING WILLAN <dadadavidopeyeminaf@gmail.com>
2023-11-30 10:09:10 +0100

    state-checkpoint

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+import React, { Component } from "react";
+
+export class App extends Component {
+  constructor() {
+    super();
+    //Implementation of state object
+    this.state = {
+      fullName: "king David Gold ",
+      bio: "IT Specialty",
+      imgSrc: "./picx.jpeg",
+      profession: "Musician",
+      show: true,
+      count: 0,
+    };
+  }
+
+  //Function for count increamentation and show checks and update
+  increamentCount = () => {
+    if (this.state.show === true) {
+      this.setState({
+        count: this.state.count + 1,
+        show: (this.state.show = false),
+      });
+    } else {
+      this.setState({
+        show: (this.state.show = true),
+      });
+    }
+  };
+  render() {
+    return (
+      <div className="container pt-5">
+        {/* Condition that checks and displays details accordingly */}
+        {this.state.show ? (
+          <Card style={{ width: "18rem" }}>
+            <Card.Img
+              width={300}
+              height={350}
+              variant="top"
+              src={this.state.imgSrc}
+            />
+            <Card.Body>
+              <Card.Title>{this.state.fullName}</Card.Title>
+              <Card.Text>{this.state.bio}</Card.Text>
+              <Card.Text>{this.state.profession}</Card.Text>
+            </Card.Body>
+          </Card>
+        ) : (
+          <h1>Nothing to show</h1>
+        )}
+        {/* button that toggles between show  */}
+        <Button
+          className="mt-3"
+          onClick={this.increamentCount}
+          variant="primary"
+        >
+          Toggle View {this.state.count}
+        </Button>
+      </div>
+    );
+  }
+}
+
+export default App;
